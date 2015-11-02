bdecf9241ffccb29f1d57f98942c664eb333c0b2
grosp4 <grosp4@bfh.ch>
2015-11-02 14:48:49 +0100

    Included alternative sigmoid function
    - new sigmoid function is faster and easier
    - uses less memory
    - is less precise
    - added file works and is verified

--- develop/Patrick/__init__.py
+++ develop/Patrick/__init__.py
@@ -1,4 +1,3 @@
 __author__ = 'Patrick'
-from Mammals import Mammals
 from Birds import Birds
 from drawing_functions import plot_data
--- develop/Patrick/drawing_functions.py
+++ develop/Patrick/drawing_functions.py
@@ -27,7 +27,7 @@ __author__ = 'Patrick'
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as numblibrary
-import os
+import os.path
 
 #  ******************************************************************************/
 # /*
